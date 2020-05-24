6f8ae3325f7ea157dd84b8eab497090abd9a35da
Ksenia <bobrovaksenia@outlook.com>
2020-05-25 01:25:38 +0200

    Main page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/:id', to: 'posts#show'
+  
+  root to: "posts#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
